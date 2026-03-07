de1e473f46e24f2c89b8fa7a0f35ebd057329dd9
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-03-08 00:11:20 +0700

    B34 1 5. Thêm mới công việc

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -10,6 +10,8 @@ router.patch("/change-status/:id", controller.changeStatus);
 
 router.patch("/change-multi", controller.changeMulti);
 
+router.post("/create", controller.create);
+
 
 
 module.exports = router
