736babb8cdc5340aa6b3550501032e3bfc5e9d03
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-03-08 12:20:15 +0700

    B35 1 1. Tính năng đăng nhập + validate

--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const controller = require("../controllers/user.controller")
-
+const authValidates = require("../validates/auth.validate")
 router.post("/register", controller.register);
+
+router.post("/login",authValidates.loginPost, controller.login);
 
 
 
---
+++ api/v1/validates/auth.validate.js
@@ -0,0 +1,17 @@
+module.exports.loginPost = (req,res,next)=>{
+  if(!req.body.email){
+    res.json({
+      message: "Vui lòng nhập email"
+    })
+    
+    return 
+  }
+  if(!req.body.password){
+    res.json({
+      message: "Vui lòng nhập mật khẩu"
+    })
+  }
+  next();
+}
+
+
