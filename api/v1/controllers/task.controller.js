299d792dac451eb6b8eb8340cec94bd39774b999
Nguyen Viet Hieu <hieusanta456@gmail.com>
2026-03-08 21:19:30 +0700

    B35 2 7. Lấy ra danh sách task theo đúng user

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -5,6 +5,10 @@ const searchHelper = require("../../../helpers/search");
 //GET /api/v1/tasks
 module.exports.index = async (req, res) => {
   const find = {
+    $or:[
+      {createdBy: req.user.id},
+      {listUser: req.user.id}
+    ],
     deleted: false,
   };
   if (req.query.status) {
